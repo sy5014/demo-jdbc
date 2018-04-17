54b670736ee03243c9b47e05691de416f256a72c
linrx1 <linrx1@wangsu.com>
2018-04-17 10:02:37 +0800

    修改测试用例生成内容

--- src/main/resources/templates/java/mybatis/JavaTest.java
+++ src/main/resources/templates/java/mybatis/JavaTest.java
@@ -1,5 +1,5 @@
 package ${table.packageName};
-<#setting number_format="#">；
+<#setting number_format="#">
 import java.io.Serializable;
 import org.apache.commons.lang3.time.DateUtils;
 import ${table.projectName}.bean.${table.className};
@@ -13,13 +13,13 @@ public class ${table.className}Test {
 		${table.className} ${table.className?uncap_first} = new ${table.className}();
 		<#list table.columnList as column>
 		<#if column.javaType == 'String'>
-		${table.className?uncap_first}.set${column.capitalizeCamelField}("${column.value?default('undefined')}");
+		${table.className?uncap_first}.set${column.capitalizeCamelField}("${column.value?default('"undefined"')}");
 		<#elseif column.javaType == 'BigDecimal'>
 		${table.className?uncap_first}.set${column.capitalizeCamelField}(${column.value?default(0)});
 		<#elseif column.javaType == 'Date'>
-		${table.className?uncap_first}.set${column.capitalizeCamelField}(DateUtils.parseDate(${column.value?default(0)}, "yyyy-MM-dd HH:mm:ss"));
+		${table.className?uncap_first}.set${column.capitalizeCamelField}(DateUtils.parseDate("${column.value?default(0)}", "yyyy-MM-dd HH:mm:ss"));
 		<#else>
-		${table.className?uncap_first}.set${column.capitalizeCamelField}("${column.value?default('undefined')}");
+		${table.className?uncap_first}.set${column.capitalizeCamelField}(${column.value?default('"undefined"')});
 		</#if>
 		</#list>
 	}
